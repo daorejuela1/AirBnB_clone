de55b415c3ac600a2fb0c0f5f80601a20a3fae09
bryano13 <bryano13@hotmail.com>
2020-06-30 22:41:16 -0500

    test it with self instead of class attribute

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,25 +13,23 @@ class FileStorage():
 
     def all(self):
         """ returns the dictionaries """
-        return (self.__objects)
+        return (FileStorage.__objects)
 
     def new(self, obj):
         """ create a new object """
         class_name = type(obj).__name__
         my_id = obj.id
         instance_key = class_name + "." + my_id
-        self.__objects[instance_key] = obj.to_dict()
+        FileStorage.__objects[instance_key] = obj.to_dict()
 
     def save(self):
         """ saves in json format to a file """
         with open(FileStorage.__file_path, 'w') as file_path:
-            file_path.write(json.dumps(self.__objects))
+            file_path.write(json.dumps(FileStorage.__objects))
 
     def reload(self):
         """ loads from json file """
         if not os.path.isfile(FileStorage.__file_path):
             return
         with open(FileStorage.__file_path, "r") as file_path:
-            my_data = file_path.read()
-            if (my_data):
-                self.__objects = json.loads(my_data)
+            FileStorage.__objects = json.load(file_path)
